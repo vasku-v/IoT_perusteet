b58a9232c1c656ecb3f317ebec0241f3ff7c0a20
vasku-v <vasilisa.vilkki@gmail.com>
2025-09-22 14:12:32 +0300

    First commit

---
+++ Viikko1/t1_ask_name/main.py
@@ -0,0 +1,7 @@
+name = ""
+
+name = input("What is your name? ")
+if name.lower() == ("clark kent"):
+  print("You are the Superman!")
+else:
+  print("You are an ordinary person.")
---
+++ Viikko1/t2_print_numbers/main.py
@@ -0,0 +1,5 @@
+print("Ready to go!")
+print("Loop starting")
+for i in range(10):
+  print("Loop number:", i)
+print("Loop finished")
